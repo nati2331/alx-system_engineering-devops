6b6b2d91ed380a5c74774911d73de6b238db8c43
nati2331 <natnael.mk2@gmail.com>
2024-05-07 08:36:08 -0700

    thre

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -6,11 +6,12 @@ import requests
 
 def number_of_subscribers(subreddit):
     """function that queries the Reddit API"""
-    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
-    headers = {'User-Agent': 'My User Agent 1.0'}
-    response = requests.get(url, headers=headers)
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {"User-Agent": "Mozilla/5.0"}
+    response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code == 200:
         data = response.json()
-        return data.get('data').get('subscribers')
+        subscribers = data['data']['subscribers']
+        return subscribers
     else:
         return 0
