0cb3671e30c960157b72856474785008ad44cb7f
nati2331 <natnael.mk2@gmail.com>
2024-05-07 08:21:24 -0700

    first

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+    script that queries the Reddit API
+"""
+
+
+import requests
+
+
+base_url = 'https://www.reddit.com/'
+
+
+def number_of_subscribers(subreddit):
+    """
+        return the number of subscribers
+    """
+
+    try:
+        response = requests.get(
+            url="{}/r/{}/about.json".format(base_url, subreddit),
+            headers={'user-agent': 'APP-NAME by REDDIT-USERNAME'},
+        )
+
+        data = response.json()['data']
+
+        return data['subscribers']
+    except Exception:
+        return False
