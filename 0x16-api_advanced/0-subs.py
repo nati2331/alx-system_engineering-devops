a1132d5f6125e5d399830d20e96ba5635cb8f98b
nati2331 <natnael.mk2@gmail.com>
2024-08-13 19:34:19 +0000

    third

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,16 +1,27 @@
 #!/usr/bin/python3
-" The fuction returns number of subscribers"
+"""
+returns the number of subscribers for a given subreddit.
+"""
 
 import requests
 
 def number_of_subscribers(subreddit):
-    """returns number of subscribers"""
+    """Returns the number of subscribers for a given subreddit."""
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {
         "User-Agent": "Nate request"
     }
-    res = requests.get(url, headers=headers, allow_redirects=False)
-    if res.status_code == 404:
+    try:
+        response = requests.get(url, headers=headers, allow_redirects=False)
+        
+        if response.status_code == 200:
+            data = response.json().get("data", {})
+            return data.get("subscribers", 0)
+        elif response.status_code == 404:
+            return 0
+        else:
+            return 0
+    
+    except requests.RequestException:
         return 0
-    results = res.json().get("data")
-    return results.get("subscribers")
+
--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,21 +1,43 @@
 #!/usr/bin/python3
-"""prints titles of the first 10 hot posts"""
+"""
+Prints titles of the first 10 hot posts listed for a given subreddit.
+"""
 
 import requests
 
-
 def top_ten(subreddit):
     """
-        prints titles of the first 10 hot posts
+    Prints the titles of the first 10 hot posts for a given subreddit.
+    If the subreddit is invalid, prints None.
     """
     url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
-    headers = {'user-agent': 'Nate request'}
-    req = requests.get(url, headers=headers)
-    if (req.status_code == 404):
-        print("None")
-    elif 'data' not in req.json():
-        print("None")
-    else:
-        req = req.json()
-        for post in req['data']['children']:
+    headers = {'User-Agent': 'Nate request'}
+    
+    try:
+        response = requests.get(url, headers=headers, allow_redirects=False)
+        
+        if response.status_code == 404:
+            print("None")
+            return
+        
+        if not response.text:
+            print("None")
+            return
+        
+        try:
+            data = response.json()
+        except ValueError:
+            print("None")
+            return
+        
+        if 'data' not in data or 'children' not in data['data']:
+            print("None")
+            return
+        
+        for post in data['data']['children']:
             print(post['data']['title'])
+    
+    except requests.RequestException as e:
+        print("None")
+        return
+
