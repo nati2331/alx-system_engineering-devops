c17334501d3b29cc526c94459de5d0030d59a131
nati2331 <natnael.mk2@gmail.com>
2024-08-13 19:40:25 +0000

    five

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,23 +1,26 @@
 #!/usr/bin/python3
 """
-Prints titles of the first 10 hot posts listed for a given subreddit.
+Prints the titles of the first 10 hot posts.
 """
 
-import requests
 
 def top_ten(subreddit):
-    """
-    Prints the titles of the first 10 hot posts for a given subreddit.
-    If the subreddit is invalid, prints None.
-    """
-    url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
-    headers = {'user-agent': 'Nate request'}
-    req = requests.get(url, headers=headers)
-    if (req.status_code == 404):
+    """prints the titles of the first 10 hot post."""
+
+    import requests
+    import sys
+
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    headers = {'User-Agent': 'Mozilla/5.0'}
+    try:
+        response = requests.get(url, headers=headers,
+                                allow_redirects=False)
+        if response.status_code == 200:
+            children = response.json().get('data').get('children')
+            for i in range(10):
+                print(children[i].get('data').get('title'))
+        else:
+            print("None")
+    except Exception:
         print("None")
-    elif 'data' not in req.json():
-        print("None")
-    else:
-        req = req.json()
-        for post in req['data']['children']:
-            print(post['data']['title'])
+
