07f9d3e464a4c8989b8e20c0855a79c4578a7c37
nati2331 <natnael.mk2@gmail.com>
2024-08-13 18:54:18 +0000

    first

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+" The fuction returns number of subscribers"
+
+import requests
+
+def number_of_subscribers(subreddit):
+    """returns number of subscribers"""
+    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
+    headers = {'User-Agent': 'My User Agent 1.0'}
+    response = requests.get(url, headers=headers)
+    if response.status_code == 200:
+        data = response.json()
+        return data.get('data').get('subscribers')
+    else:
+        return 0
