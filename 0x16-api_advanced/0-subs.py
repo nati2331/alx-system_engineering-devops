412add606505046ab8e3a429e18ed787474ae380
nati2331 <natnael.mk2@gmail.com>
2024-08-14 09:15:07 +0000

    there

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -2,21 +2,18 @@
 """
 Returns the number of subscribers
 """
+
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """ Function that GETS subscriber count """
+    """ Function that GETS subscriber count of a given subreddit """
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {
         "User-Agent": "Nate request"
     }
     res = requests.get(url, headers=headers, allow_redirects=False)
-    if res.status_code != 200:
+    if res.status_code == 404:
         return 0
-    
-    try:
-	results = res.json().get("data")
-	return results.get("subscribers", 0)
-    except Exception:
-	return 0
+    results = res.json().get("data")
+    return results.get("subscribers")
