0f2b6282de052485cdf1e5fddcf45185abc346ba
nati2331 <natnael.mk2@gmail.com>
2024-08-14 09:24:26 +0000

    fivt

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,39 +1,20 @@
 #!/usr/bin/python3
 """
-Prints titles of the first 10 hot posts listed for a given subreddit.
+prints the titles
 """
-
 import requests
-def top_ten(subreddit)
-    """
-    Prints the titles of the first 10 hot posts for a given subreddit.
-    """
+
+
+def top_ten(subreddit):
+    """ Function that GETs subreddit top 10 hot posts """
     url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
-    headers = {'User-Agent': 'Nate request'}
-    try:
-        response = requests.get(url, headers=headers, allow_redirects=False)
-        # Check for HTTP status codes
-        if response.status_code == 404:
-            print("None")
-            return
-        # Ensure the response content is not empty
-        if not response.text:
-            print("None")
-            return
-        # Attempt to parse JSON
-        try:
-            data = response.json()
-        except ValueError:
-            print("None")
-            return
-        # Check if 'data' and 'children' keys exist
-        if 'data' not in data or 'children' not in data['data']:
-            print("None")
-            return
-        # Print the titles of the first 10 hot posts
-        for post in data['data']['children']:
-            print(post['data']['title'])
-    except requests.RequestException as :
-        # Handle any other exceptions
+    headers = {'user-agent': 'Nate request'}
+    req = requests.get(url, headers=headers)
+    if (req.status_code == 404):
         print("None")
-        return
+    elif 'data' not in req.json():
+        print("None")
+    else:
+        req = req.json()
+        for post in req['data']['children']:
+            print(post['data']['title'])
